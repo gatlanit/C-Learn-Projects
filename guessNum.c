36a089cf86396389fbf015275dd6e2913971d0e0
Nitin Gatla <gatlanit@gmail.com>
2024-11-10 22:22:21 -0600

    Added basic implementation of the number guessing game (needs a couple more features like the consequence as well as a dialougue warning popup and a list of all files and their respective locations)

--- guessNum.c
+++ guessNum.c
@@ -2,9 +2,42 @@
 #include <stdlib.h>
 #include <time.h>
 
-int main() {
+// TODO:
+// [] Add delete functionality
 
-  printf("Hello World!");
+int main() {
+  // Randomize Seed
+  srand(time(0));
+
+  int MIN = 1;
+  int MAX = 100;
+
+  int randNum = (rand() % MAX) + MIN;
+  int guessNum;
+  int guesses = 0;
+
+  printf("Guess a number between 1-100! (everytime you get it wrong, a random "
+         "file gets deleted :3)\n");
+
+  do {
+    printf("Enter a guess: "); // Input
+    scanf("%d", &guessNum);
+
+    if (guessNum > randNum) { // Hints
+      printf("Guess is too high\n\n");
+      guesses++;
+    } else if (guessNum < randNum) {
+      printf("Guess is too low\n\n");
+      guesses++;
+    } else {
+      printf("That's correct!\n\n");
+    }
+
+  } while (guessNum != randNum);
+
+  printf("Congratuations you win!\n");
+  printf("The answer was %d\n", randNum);
+  printf("Numbner of files deleted: %d\n", guesses);
 
   return 0;
 }
